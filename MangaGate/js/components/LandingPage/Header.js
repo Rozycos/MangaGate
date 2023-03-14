4de9c7ab161041deee4b306832c1885719568751
rozycos <rozycos@gmail.com>
2023-03-15 00:54:33 +0100

    created hero section, renamed slider section to hero section

--- MangaGate/js/components/LandingPage/Header.js
+++ MangaGate/js/components/LandingPage/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import Hamburger from "./Hamburger";
 import Navigation from "./Navigation";
-import Slider from "./Slider";
+import Hero from "./Hero";
 
 const Header = () => {
 
@@ -11,7 +11,7 @@ const Header = () => {
         <>
             <header className="header container">
                 <Navigation/>
-                <Slider/>
+                <Hero/>
             </header>
         </>
     )
