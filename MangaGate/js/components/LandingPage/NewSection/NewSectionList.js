882da08752c552732e00b93414d06a863cfd94cb
rozycos <rozycos@gmail.com>
2023-03-26 23:30:04 +0200

    created function showed post category

--- MangaGate/js/components/LandingPage/NewSection/NewSectionList.js
+++ MangaGate/js/components/LandingPage/NewSection/NewSectionList.js
@@ -3,13 +3,25 @@ import { Link, NavLink, Outlet } from "react-router-dom";
 
 const NewSectionList = (props) =>{
     const { articles, width } = props
-    const [open, setOpen] = useState(false);
+    //const [open, setOpen] = useState(false);
+    
+    const articlesCategory =[
+        {name: "action", status: articles.action}, 
+        {name: "comedy", status:articles.comedy}, 
+        {name: "drama", status: articles.drama}, 
+        {name: "fantasy", status: articles.fantasy}, 
+        {name: "military", status:articles.military}
+    ];
+
+    const singleArticleCategory = articlesCategory.filter((e)=> e.status===true);
+    const showCategory = singleArticleCategory.map(({name})=> name).join(", ");
+    //console.log(singleArticleCategory, {...showCategory})
 
     return(
         <span className="new__box new__box--container new__box--span">
                     <img src={articles.imageURL}/>
                     <div className="new__box--text">
-                        <span>Fantasy</span>
+                        <span>{showCategory}</span>
                         <h3>{articles.title}</h3>
                         <p>{articles.postText}</p>
                         {/* <a className="new__box--text-link">Read Full &#187;</a> */}
