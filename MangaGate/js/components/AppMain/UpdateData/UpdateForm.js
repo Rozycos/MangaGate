3518740d92872627c2438e04e47d4b86bfeea3d4
rozycos <rozycos@gmail.com>
2023-03-30 22:10:39 +0200

    created edit article feature

--- MangaGate/js/components/AppMain/UpdateData/UpdateForm.js
+++ MangaGate/js/components/AppMain/UpdateData/UpdateForm.js
@@ -70,13 +70,8 @@ const UpdateForm = () => {
 const { imageURL, title, subtitle, postURL, postText} = inputsContent; 
 
     const handleInputChange = e => {
-        // if({[e.target.name]: e.target.value} === ''){
-        //     setInputsContent(singleArticle.imageURL)
-        // } else
-        // !inputsContent.title? e.preventDefault() :setInputsContent({
-        //          [e.target.name]: e.target.value
-        //      });
-
+        // if(e.target.value === ''){alert("nie ma czego zapisać"); console.log(e.target.value)}
+        //     else
         setInputsContent({
             [e.target.name]: e.target.value
         });
@@ -110,18 +105,93 @@ const { imageURL, title, subtitle, postURL, postText} = inputsContent;
         setOther(!other);
       };
 
-    const handleSubmit = async (e) => {
+    const handleSubmitImageURL = async (e) => {
         e.preventDefault()
-
-console.log("single" + singleArticle)
 
         try {
           await updateDoc(docRef, {
             imageURL: imageURL,
+            // title: title,
+            // subtitle: subtitle,
+            // postURL: postURL,
+            // postText: postText,
+            // isPublish: isPublish,
+            // action: action,
+            // comedy: comedy,
+            // drama: drama,
+            // military: military,
+            // fantasy: fantasy,
+            // other: other,
+            //created: Timestamp.now()
+          })
+
+          alert("Your data is updated!")
+        } catch (err) {
+          alert(err)
+        }
+    }
+
+    const handleSubmitTitle = async (e) => {
+        e.preventDefault()
+
+        try {
+          await updateDoc(docRef, {
             title: title,
+          })
+
+          alert("Your data is updated!")
+        } catch (err) {
+          alert(err)
+        }
+    }
+
+    const handleSubmitSubtitle = async (e) => {
+        e.preventDefault()
+
+        try {
+          await updateDoc(docRef, {
             subtitle: subtitle,
+          })
+
+          alert("Your data is updated!")
+        } catch (err) {
+          alert(err)
+        }
+    }
+
+    const handleSubmitPostURL = async (e) => {
+        e.preventDefault()
+
+        try {
+          await updateDoc(docRef, {
             postURL: postURL,
+          })
+
+          alert("Your data is updated!")
+        } catch (err) {
+          alert(err)
+        }
+    }
+
+    const handleSubmitPostText = async (e) => {
+        e.preventDefault()
+
+        try {
+          await updateDoc(docRef, {
             postText: postText,
+          })
+
+          alert("Your data is updated!")
+        } catch (err) {
+          alert(err)
+        }
+    }
+
+    const handleCheckboxesSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+          await updateDoc(docRef, {
             isPublish: isPublish,
             action: action,
             comedy: comedy,
@@ -129,23 +199,14 @@ console.log("single" + singleArticle)
             military: military,
             fantasy: fantasy,
             other: other,
-            //created: Timestamp.now()
           })
 
           alert("Your data is updated!")
-          navigate('/admin');
         } catch (err) {
           alert(err)
         }
     }
 
-    const fillTextValue = () =>{
-       const fillText = "";
-        console.log("data not changed");
-       return fillText = " ";
-    }
-    console.log(singleArticle.e)
-    console.log(singleArticle.postURL + " " + articleId + "  " + `${singleArticle.imageURL}` + singleArticle.isPublish)
     return (
         <>
             <div className="login__form">
@@ -156,27 +217,42 @@ console.log("single" + singleArticle)
                         <input className="form__input" type="text" name="imageURL" id="imageURL" placeholder="Enter image URL" onChange={handleInputChange} defaultValue={singleArticle.imageURL || ''}/> 
                         {/* <input className="form__input" type="text" name="imageURL" id="imageURL" placeholder="Enter image URL" onChange={handleInputChange}/> */}
                     </div>
+                    <button className="btn btn__primary btn__form" disabled={!inputsContent.imageURL}  onClick={handleSubmitImageURL}>Update
+                        {/* <Link className="admin__link" to="/admin">Submit</Link> */}
+                    </button>
                     <div className="form__field">
                         <label className="form__label">Title</label>
                         <input className="form__input" type="text" name="title" id="title" placeholder="Enter post title" onChange={handleInputChange} defaultValue={singleArticle.title || ''}/>
                     </div>
+                    <button className="btn btn__primary btn__form" disabled={!inputsContent.title}  onClick={handleSubmitTitle}>
+                        Update
+                    </button>
                     <div className="form__field">
                         <label className="form__label">Subtitle</label>
                         <input className="form__input" type="text" name="subtitle" id="subtitle" placeholder="Enter post subtitle" onChange={handleInputChange} defaultValue={singleArticle.subtitle || ''}/>
                     </div>
+                    <button className="btn btn__primary btn__form" disabled={!inputsContent.subtitle}  onClick={handleSubmitSubtitle}>
+                        Update
+                    </button>
                     <div className="form__field">
                         <label className="form__label">Post URL</label>
                         <input className="form__input" type="text" name="postURL" id="postURL" placeholder="Enter post URL" onChange={handleInputChange} defaultValue={singleArticle.postURL || ''}/>
                     </div>
-                    <div className="form__field--textarea">
+                    <button className="btn btn__primary btn__form" disabled={!inputsContent.postURL}  onClick={handleSubmitPostURL}>
+                            Update
+                    </button>
+                    <div className="form__field--textarea form__field--textarea-update">
                         <label className="form__label" >Post text</label>
-                        <textarea className="form__input form__input--textarea" name="postText" id="postText" rows="5" cols="33" placeholder="Article" onChange={handleInputChange} defaultValue={singleArticle.postText || ''} />
+                        <textarea className="form__input " name="postText" id="postText" rows="5" cols="33" placeholder="Article" onChange={handleInputChange} defaultValue={singleArticle.postText || ''} />
                     </div>  
-                    <div className="form__field--checkbox">
+                    <button className="btn btn__primary btn__form btn__form--update" disabled={!inputsContent.postText}  onClick={handleSubmitPostText}>
+                        Update
+                    </button>
+                    <div className="form__field--checkbox form__field-update">
                         <label className="form__label" >Published</label>
                         <input type="checkbox" id="post__publish" name="post__publish" defaultChecked={singleArticle.isPublish} onClick={handleChange} />
                     </div>
-                    <div className="form__field">
+                    <div className="form__field form__field-update">
                         <label className="form__label" >Action</label>
                         <input type="checkbox" id="post__categories" name="post__categories" defaultChecked={singleArticle.action} onChange={handleChangeOnAction} />
                         <label className="form__label" >Comedy</label>
@@ -184,7 +260,7 @@ console.log("single" + singleArticle)
                         <label className="form__label" >Drama</label>
                         <input type="checkbox" id="post__categories" name="post__categories" defaultChecked={singleArticle.drama} onChange={handleChangeOnDrama} />
                     </div>
-                    <div className="form__field">
+                    <div className="form__field form__field-update">
                         <label className="form__label" >Military</label>
                         <input type="checkbox" id="post__categories" name="post__categories" defaultChecked={singleArticle.military} onChange={handleChangeOnMilitary} />
                         <label className="form__label" >Fantasy</label>
@@ -192,14 +268,15 @@ console.log("single" + singleArticle)
                         <label className="form__label" >Other</label>
                         <input type="checkbox" id="post__categories" name="post__categories" defaultChecked={singleArticle.other} onChange={handleChangeOnOther} />
                     </div>
+                    <button className="btn btn__primary btn__form" onClick={handleCheckboxesSubmit}>
+                        Update
+                    </button>
                 </form>
                 <div>
-                    <button className="btn btn__primary btn__form btn__form--log">
-                    {/* </button><button className="btn btn__primary btn__form btn__form--log" onClick={handlePreview}> */}
-                            Previev
-                    </button>
-                    <button className="btn btn__primary btn__form btn__form--log"  disabled={!inputsContent.title || !inputsContent.subtitle || !inputsContent.postURL} onClick={handleSubmit}>Update
-                        {/* <Link className="admin__link" to="/admin">Submit</Link> */}
+                    {/* <button className="btn btn__primary btn__form btn__form--log"> */}
+                    {/* </button><button className="btn btn__primary btn__form btn__form--log" onClick={handlePreview}> Previev</button>*/}
+                    <button className="btn btn__primary btn__form btn__form--log" >
+                        <Link className="admin__link" to="/admin">Back to admin panel</Link>
                     </button>
                 </div>
             </div>
@@ -212,4 +289,4 @@ console.log("single" + singleArticle)
 
 export default UpdateForm;
 
-// disabled={!inputsContent.title || !inputsContent.subtitle || !inputsContent.postURL}
+// disabled={!inputsContent.title || !inputsContent.subtitle || !inputsContent.postURL} onClick={handleSubmit}
